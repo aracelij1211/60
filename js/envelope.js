566267a8e24cca5c226f4445e291d4c64b680888
David Márquez <dmh.david@gmail.com>
2025-10-01 19:19:26 -0600

    update

---
+++ js/envelope.js
@@ -0,0 +1,40 @@
+var envelope = document.getElementById('envelope');
+var btn_open = document.getElementById("open");
+
+function fadeout(){
+  var el =document.getElementById('env-init');
+  var elMain =document.getElementById('all');
+  fadeOutElement(el,elMain);
+}
+
+function fadeOutElement(el, elMain) {
+  var opacity = 1; // Initial opacity
+  var show = 0;
+  elMain.style.display='';
+  var interval = setInterval(function() {
+     if (opacity > 0) {
+        opacity -= 0.1;
+        show += 0.1;
+        el.style.opacity = opacity;
+        elMain.opacity = show;
+     } else {
+        clearInterval(interval); // Stop the interval when opacity reaches 0
+        el.style.display = 'none'; // Hide the element
+        elMain.style.opacity= 1;
+        audio.play();
+     }
+  }, 53);
+}
+
+
+function open() {
+  //envelope.classList.add("open");
+  //envelope.classList.remove("close");
+  setTimeout(fadeout, 2500)
+}
+
+btn_open.addEventListener("click", () => {
+  open();
+},
+  false
+);
